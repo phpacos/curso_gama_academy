b3e3facc5f38628b69e538d85e89e52cbb8e27c9
phpacos <59850400+phpacos@users.noreply.github.com>
2020-09-03 13:24:59 -0300

    adicionado template_string

---
+++ java_script/template_string.js
@@ -0,0 +1,14 @@
+let meuNome = 'PH';
+let meuSobrenome = 'PACOS';
+let minhaProfisssao = 'DEV';
+
+
+console.log(`Olá meu nome é ${meuNome} ${meuSobrenome} e minha profissão é: ${minhaProfisssao}`);
+
+console.log(`O resultado da soma de 1 + 1 é igual a ${1 + 1}`);
+
+console.log(`O objeto json ${{chave:'valor'}}`);
+
+let meuObjeto = {
+    chave: 'novo valor'
+}
